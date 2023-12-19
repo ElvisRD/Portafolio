b34ad74e94547b842c4c22bb7df0d88c02f76030
erodriguez <elvis.rodriguez@engagemeapps.com>
2023-12-18 23:18:33 -0300

    se movio informacion a nueva capeta llamada secciones para tener un orden mejor dentro de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
-import { SkillsComponent } from './components/skills/skills.component';
+import { HomeComponent } from './sections/home/home.component';
+import { AboutMeComponent } from './sections/about-me/about-me.component';
+import { SkillsComponent } from './sections/skills/skills.component';
 import { CardProjectComponent } from './components/card-project/card-project.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './sections/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconTechComponent } from './components/icon-tech/icon-tech.component';
 import { IconLinkComponent } from './components/icon-link/icon-link.component';
-import { AplicationsComponent } from './components/aplications/aplications.component';
+import { AplicationsComponent } from './sections/aplications/aplications.component';
 
 @NgModule({
   declarations: [
