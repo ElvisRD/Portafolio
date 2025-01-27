217ebd4a0b35bf2b6971e712113544569dcede07
ElvisRD <animeamv682@gmail.com>
2025-01-27 18:07:16 -0300

    se optimizo la pagina web

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -11,28 +11,17 @@ export class NavBarComponent {
   constructor(public sanitizer: DomSanitizer, private pageScrollService: PageScrollService) {}
 
   menu: boolean = false;
-  play: boolean = false;
-
   svgIcon: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/nav/cat.svg');
-  svgPlay: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/nav/play.svg');
-  svgPause: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/nav/pause.svg');
-  music: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl('/assets/music/music.mp3');
 
-  playMusic(){
-    this.play = !this.play;
-  }
   mostrarMenu() {
     this.menu = !this.menu;
   }
 
   cerrarMenu(target: string) {
     this.menu = false;
-    
     this.pageScrollService.scroll({
       document: document,
       scrollTarget: target,
     });
   }
-
- 
 }
